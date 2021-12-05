21157aad84a30e70ba0d38bf43472d7840fe6300
evgen <belyevevgen@mail.ru>
2021-12-05 21:24:59 +0300

    5-12-21

--- parser_knig.py
+++ parser_knig.py
@@ -1,5 +1,7 @@
 import requests as rq
 from bs4 import BeautifulSoup as bfs
+import pymysql
+from getpass import getpass
 
 def ppr_kol(siklHref):
     # url=knigi[0]['sikl_href']
@@ -16,6 +18,20 @@ soup=bfs(r.text,'html.parser')
 npp=1
 items=soup.find_all('div',class_='short-item')
 knigi=[]
+#    Подключение MySQL
+con = pymysql.connect(host='localhost',
+    user='root',
+    password=getpass("Пароль: "),
+    db='parser_knig',
+    charset='utf8mb4',
+    cursorclass=pymysql.cursors.DictCursor)
+# try:
+#     with con as connection:
+#         print(connection)
+cur = con.cursor()
+
+
+
 for item in items:
     # soupit=bfs(item,'html.parser')
     ul=item.find('ul',class_='short-list')
@@ -26,21 +42,43 @@ for item in items:
         # next(items)
         print("\nNext\n",item.find('a',class_='short-title').get_text(),"\n\n")
         continue
+    li1=lis[1]
+    a1=li1.find('a')
+    avtor=a1.get_text()
+
     li=lis[4]
     a=li.find('a')
+    sikl=a.get_text()
+    
+    sikl_count=ppr_kol(a.get('href'))
+    
     h=li.find('a').get('href')
+    # print(lis[1].get_text())
+    wsSql="SELECT * FROM knigi WHERE avtor='"+avtor+"' and cikl='"+sikl+"'"
+    print(wsSql)
+    cur.execute(wsSql)
+    # cur.execute("SELECT * FROM knigi") SELECT * FROM knigi where avtor='belyev' and cikl='home'
+    # cur.execute("SELECT * FROM knigi WHERE avtor='%s' and cikl='%s'", lis[1].get_text(),lis[4].get_text())
+    # cur.execute("SELECT * FROM knigi WHERE avtor='belyev' and cikl='home11'")
+    if cur.rowcount<1 :
+        wsSqlIns="INSERT INTO knigi (avtor,cikl,kol,flag) VALUES ('"+avtor+"','"+sikl+"',"+str(sikl_count)+",0)"
+        cur.execute(wsSqlIns)
+        # cur.commit()
+    # rows  = cur.fetchall()
+    # for row in rows:
+    #     print(row['avtor'],row['cikl'])
     knigi.append(
         {
             'npp':npp,
             'title':item.find('a',class_='short-title').get_text(),
              'god':lis[0].get_text(),
-             'avtor':lis[1].get_text(),
+             'avtor':avtor,
              'akter':lis[2].get_text(),
              'time':lis[3].get_text(),
-             'sikl':lis[4].get_text(),
+             'sikl':sikl,
              'sikl_href':a.get('href'),
              'sikl_npp':li.contents[-1],
-             'sikl_count':ppr_kol(a.get('href'))
+             'sikl_count':sikl_count
         }
     )
     # print (npp)
@@ -51,7 +89,7 @@ for item in items:
 # for ws in knigi:
 #     print(ws)
 for ws in knigi:
-    print(ws['npp'],ws['sikl'],ws['sikl_npp'],ws['sikl_count'])
+    print(ws['npp'],ws['avtor'],ws['sikl'],ws['sikl_npp'],ws['sikl_count'])
 
 print(len(knigi))
 print(knigi[0]['npp'])
@@ -60,3 +98,6 @@ r=rq.get(url)
 soup=bfs(r.text,'html.parser')
 items=soup.find_all('div',class_='short-item')
 print(len(items))
+
+cur.close()
+con.commit()
--- parser_knigi_sql.py
+++ parser_knigi_sql.py
@@ -9,17 +9,19 @@ con = pymysql.connect(host='localhost',
     db='parser_knig',
     charset='utf8mb4',
     cursorclass=pymysql.cursors.DictCursor)
-try:
-    with con as connection:
-        print(connection)
-        cur = con.cursor()
-        
-        cur.execute("SELECT * FROM knigi") 
-        # row  = cur.fetchone()
-        rows  = cur.fetchall()
-        for row in rows:
-            print(row['avtor'],row['cikl'])
-except pymysql.Error as e:
-    print(e)
-else :
-    cur.close()
+# try:
+#     with con as connection:
+#         print(connection)
+cur = con.cursor()
+
+cur.execute("SELECT * FROM knigi") 
+# row  = cur.fetchone()
+rows  = cur.fetchall()
+for row in rows:
+    print(row['avtor'],row['cikl'])
+# except pymysql.Error as e:
+#     print(e)
+# else :
+cur.execute("DELETE FROM knigi") 
+cur.close()
+con.commit()
